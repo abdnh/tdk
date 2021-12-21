e0dd6781880d0390dc964d630189ebaf6364f846
Abdo <abd.nh25@gmail.com>
2021-12-22 01:21:25 +0300

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="TDK",
+    version="1.0",
+    description="Get Turkish word definitions from the TDK dictionary",
+    author="Abdo",
+    author_email="abd.nh25@gmail.com",
+    url="https://github.com/abdnh/tdk",
+    py_modules=["tdk"],
+)
